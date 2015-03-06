9512b06395b4afa2cea7fd1b21ea5c7946916c73
rikniitt <riku.niittymaki@gmail.com>
2015-03-06 19:43:47 +0200

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tvNytApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tvNytApp
+ */
+angular.module('tvNytApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tvNytApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tvNytApp
+ */
+angular.module('tvNytApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
